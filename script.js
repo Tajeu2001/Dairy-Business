4bfd9289e1fcb346142e0a4e45dea00a2bbcb026
Tajeu2001 <tajeusanta@gmail.com>
2021-03-14 16:51:53 +0300

    Create a dailyMilkProduction function

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-var
-}
+const dailyMilkProduction = function (cows ,litres) { 
+    console.log(cows*litres);
+} 
+const shedOneDaily = dailyMilkProduction(50, 20);
+const shedTwoDaily = dailyMilkProduction(45, 18);
+const shedThreeDaily = dailyMilkProduction(48, 24);
+const shedFourDaily = dailyMilkProduction(55, 22);
+
+let totalDailyProduction= shedOneDaily + shedTwoDaily + shedThreeDaily + shedFourDaily;
+console.log(totalDailyProduction);
+
+const incomeOvertime
+let dailyIncome =totalDailyproduction *
